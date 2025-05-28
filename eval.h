d5e597839adb4d45c126f68de26dd3c2807c0e63
Mellow <nothornypitou@outlook.com>
2025-05-28 15:12:23 -0300

    Fixed missing function

--- eval.h
+++ eval.h
@@ -4,6 +4,7 @@
 #include "ast.h"
 #include "hashmap.h"
 
+Value eval_unary_op(Value expr, const char *op);
 Value eval_binary_op(Value left, Value right, const char *op);
 Value eval(ASTNode *node, HashMap *env);
 
