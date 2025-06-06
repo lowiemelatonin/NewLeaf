62f9976b1c9011f89a1064d9b49f5e76f47132d4
Mellow <nothornypitou@outlook.com>
2025-06-06 11:45:45 -0300

    Add New Data Types

--- ast.h
+++ ast.h
@@ -3,18 +3,28 @@
 
 typedef enum {
     IDENTIFIER_NODE,
+    ASSIGNMENT_NODE,
 
+    SHORT_NODE,
+    UNSIGNED_SHORT_NODE,
     INT_NODE,
-    DOUBLE_NODE,
+    UNSIGNED_INT_NODE,
+    LONG_NODE,
+    UNSIGNED_LONG_NODE,
+    LONG_LONG_NODE,
     FLOAT_NODE,
+    DOUBLE_NODE,
+    SIGNED_CHAR_NODE,
     CHAR_NODE,
+    UNSIGNED_CHAR_NODE,
     STRING_NODE,
+    POINTER_NODE,
+    VOID_NODE,
 
     UNARY_OPERATION_NODE,
     BINARY_OPERATION_NODE,
-    TERNARY_OPERATION_NODE,
+    TERNARY_OPERATION_NODE
 
-    ASSIGNMENT_NODE
 } NodeType;
 
 struct ASTNode;
@@ -27,24 +37,69 @@ typedef struct ASTNode {
         } identifier;
 
         struct {
+            ASTNode *left;
+            ASTNode *right;
+        } assignment;
+
+        struct {
+            short value;
+        } shortLiteral;
+
+        struct {
+            unsigned short value;
+        } unsignedShortLiteral;
+
+        struct {
             int value;
         } intLiteral;
 
         struct {
-            double value; 
-        } doubleLiteral;
+            unsigned int value;
+        } unsignedIntLiteral;
+
+        struct {
+            long value;
+        } longLiteral;
+
+        struct {
+            unsigned long value;
+        } unsignedLongLiteral;
+
+        struct {
+            long long value;
+        } longLongLiteral;
+
+        struct {
+            unsigned long long value;
+        } unsignedLongLongLiteral;
 
         struct {
             float value;
         } floatLiteral;
 
         struct {
+            double value; 
+        } doubleLiteral;
+
+        struct {
+            signed char value;
+        } signedCharLiteral;
+
+        struct {
             char value;
         } charLiteral;
 
         struct {
+            unsigned char value;
+        } unsignedCharLiteral;
+
+        struct {
             char *value;
         } stringLiteral;
+        
+        struct {
+            ASTNode *ptr;
+        } pointer;
         
         struct {
             ASTNode *expr;
@@ -62,11 +117,7 @@ typedef struct ASTNode {
             ASTNode *true_expr;
             ASTNode *false_expr;
         } ternaryOp;
-        
-        struct {
-            ASTNode *left;
-            ASTNode *right;
-        } assignment;
+
     };
 
 } ASTNode;
