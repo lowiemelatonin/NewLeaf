eb065eca7c90ab2ad91a1a26a4fab1018016118a
Mellow <nothornypitou@outlook.com>
2025-06-25 19:35:17 -0300

    Add files via upload

--- lexer.h
+++ lexer.h
@@ -2,58 +2,92 @@
 #define LEXER_H
 
 typedef enum {
-    TOKEN_BYTE,             // byte
-    TOKEN_BOOL,             // bool
-    TOKEN_SHORT,            // short
-    TOKEN_USHORT,           // ushort
-    TOKEN_INT,              // int
-    TOKEN_UINT,             // uint
-    TOKEN_LONG,             // long
-    TOKEN_ULONG,            // ulong
-    TOKEN_LONG_LONG,        // long long
-    TOKEN_ULONG_LONG,       // ulong long
-    TOKEN_FLOAT,            // float
-    TOKEN_DOUBLE,           // double
-    TOKEN_LONG_DOUBLE,      // long double
-    TOKEN_SIGNED_CHAR,      // schar
-    TOKEN_CHAR,             // char
-    TOKEN_UNSIGNED_CHAR,    // uchar
-    TOKEN_STRING,           // string
+    TOKEN_VOID,                 // void
+    TOKEN_BYTE,                 // byte
+    TOKEN_BOOL,                 // bool
+    TOKEN_SHORT,                // short
+    TOKEN_USHORT,               // ushort
+    TOKEN_INT,                  // int
+    TOKEN_UINT,                 // uint
+    TOKEN_LONG,                 // long
+    TOKEN_ULONG,                // ulong
+    TOKEN_LONG_LONG,            // long long
+    TOKEN_ULONG_LONG,           // ulong long
+    TOKEN_FLOAT,                // float
+    TOKEN_DOUBLE,               // double
+    TOKEN_LONG_DOUBLE,          // long double
+    TOKEN_SIGNED_CHAR,          // schar
+    TOKEN_CHAR,                 // char
+    TOKEN_UNSIGNED_CHAR,        // uchar
+    TOKEN_STRING,               // string
 
-    TOKEN_ARCH,             // arch
-    TOKEN_UNSIGNED_ARCH,    // uarch
+    TOKEN_ARCH,                 // arch
+    TOKEN_UNSIGNED_ARCH,        // uarch
 
-    TOKEN_CONST,            // const
-    TOKEN_STATIC,           // static
-    TOKEN_EXTERN,           // extern
-    TOKEN_VOLATILE,         // volatile
-    TOKEN_ATOMIC,           // atomic
+    TOKEN_CONST,                // const
+    TOKEN_STATIC,               // static
+    TOKEN_EXTERN,               // extern
+    TOKEN_VOLATILE,             // volatile
+    TOKEN_ATOMIC,               // atomic
 
-    TOKEN_IDENTIFIER,       // variable names, functions, types, params...
-    TOKEN_NUMBER,           // for numeric types
-    TOKEN_STRING_LITERAL,   // strings
+    TOKEN_IDENTIFIER,           // variable names, functions, types, params...
+    TOKEN_NUMBER,               // for numeric types
+    TOKEN_STRING_LITERAL,       // strings
     
-    TOKEN_LPAREN,           // (
-    TOKEN_RPAREN,           // )
-    TOKEN_LBRACE,           // {
-    TOKEN_RBRACE,           // }
-    TOKEN_LBRACKET,         // [
-    TOKEN_RBRACKET,         // ]
-    TOKEN_COMMA,            // ,
-    TOKEN_SEMICOLON,        // ;
-    TOKEN_COLON,            // :
-    TOKEN_DOT,              // .
-    TOKEN_ARROW,            // ->
+    TOKEN_LPAREN,               // (
+    TOKEN_RPAREN,               // )
+    TOKEN_LBRACE,               // {
+    TOKEN_RBRACE,               // }
+    TOKEN_LBRACKET,             // [
+    TOKEN_RBRACKET,             // ]
+    TOKEN_COMMA,                // ,
+    TOKEN_SEMICOLON,            // ;
+    TOKEN_COLON,                // :
+    TOKEN_DOT,                  // .
+    TOKEN_ARROW,                // ->
 
-    TOKEN_PLUS,             // +
-    TOKEN_MINUS,            // -
-    TOKEN_STAR,             // *
-    TOKEN_SLASH,            // /
-    TOKEN_PERCENT,          // %
+    TOKEN_PLUS,                 // +
+    TOKEN_MINUS,                // -
+    TOKEN_STAR,                 // *
+    TOKEN_SLASH,                // /
+    TOKEN_PERCENT,              // %
 
-    TOKEN_AND,              // &&
-    TOKEN_OR,               // ||
-    TOKEN_NOT               // !
+    TOKEN_AND,                  // &&
+    TOKEN_OR,                   // ||
+    TOKEN_NOT,                  // !
+
+    TOKEN_BITWISE_AND,          // &
+    TOKEN_BITWISE_OR,           // |
+    TOKEN_BITWISE_XOR,          // ^
+    TOKEN_BITWISE_NOT,          // ~
+    TOKEN_SHIFT_LEFT,           // <<
+    TOKEN_SHIFT_RIGHT,          // >>
+
+    TOKEN_INCREMENT,            // ++
+    TOKEN_DECREMENT,            // --
+
+    TOKEN_ASSIGNMENT,           // =
+    TOKEN_ADD_ASSIGNMENT,       // +=
+    TOKEN_SUB_ASSIGNMENT,       // -=
+    TOKEN_MUL_ASSIGNMENT,       // *=
+    TOKEN_DIV_ASSIGNMENT,       // /=
+    TOKEN_MOD_ASSIGNMENT,       // %=
+    TOKEN_BITWISE_AND_ASSIGN,   // &=
+    TOKEN_BITWISE_OR_ASSIGN,    // |=
+    TOKEN_BITWISE_XOR_ASSIGN,   // ^=
+    TOKEN_SHIFT_LEFT_ASSIGN,    // <<=
+    TOKEN_SHIFT_RIGHT_ASSIGN,   // >>=
+
+    TOKEN_EQUAL,                // ==
+    TOKEN_NOT_EQUAL,            // !=
+    TOKEN_LESS_THAN,            // <
+    TOKEN_LESS_EQUAL_THAN,      // <=
+    TOKEN_GREATER_THAN,         // >
+    TOKEN_GREATER_EQUAL_THAN,   // >=
+
+    TOKEN_QUESTION,             // ?
+    TOKEN_BACKSLASH,            // \ 
+
 } TokenType;
 
 #endif
